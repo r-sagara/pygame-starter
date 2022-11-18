6f90bf7528857529482eb382a656085f47ecf609
r-sagara <heshagrade@gmail.com>
2022-11-18 23:43:01 +0200

    handling bullets, drawing, moving and removing

--- main.py
+++ main.py
@@ -5,14 +5,25 @@ from pygame import Rect
 
 WIDTH, HEIGHT = 900, 500
 ASSETS_FOLDER_PATH = "Assets"
+MAX_BULLETS = 3
+BULLET_VELOCITY = 7
+
+FPS = 60
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 255)
-FPS = 60
+RED = (255, 0, 0)
 
 BORDER_WIDTH = 10
 BORDER_HEIGHT = HEIGHT
 BORDER_RECT = Rect(WIDTH//2 - BORDER_WIDTH//2, 0, BORDER_WIDTH, BORDER_HEIGHT)
+
+YELLOW_FIRE = pygame.USEREVENT + 1
+RED_FIRE = pygame.USEREVENT + 2
+
+YELLOW_HIT = pygame.USEREVENT + 3
+RED_HIT = pygame.USEREVENT + 4
+
 
 class Display:
     def __init__(self, width, height):
@@ -22,12 +33,19 @@ class Display:
     def add_object(self, game_obj):
         self.game_objects.append(game_obj)
 
+    def remove_object(self, game_obj):
+        if game_obj in self.game_objects:
+            self.game_objects.remove(game_obj)
+
     def draw(self):
         self.surface.fill(WHITE)
         pygame.draw.rect(self.surface, BLACK, BORDER_RECT)
         for game_obj in self.game_objects:
             #pygame.draw.rect(self.surface, BLACK, game_obj)
-            self.surface.blit(game_obj.image, (game_obj.x, game_obj.y))          
+            if game_obj.image:
+                self.surface.blit(game_obj.image, (game_obj.x, game_obj.y))
+            elif isinstance(game_obj, Rect):
+                pygame.draw.rect(self.surface, RED, game_obj)      
         pygame.display.update()
 
 
@@ -43,24 +61,22 @@ class DisplayObject(Rect):
 
     def rotate_image(self, target_angle):
         if self.image and self.angle != target_angle:
-            print(self.image.get_rect(topleft=self.topleft).center)
             rotated_image = pygame.transform.rotate(self.image, target_angle - self.angle)
             rotated_image_rect = rotated_image.get_rect(center=self.image.get_rect(topleft=self.topleft).center)
             self.image = rotated_image
             self.topleft = rotated_image_rect.topleft
             self.angle = target_angle
-            print(self.image.get_rect(topleft=self.topleft).center)
    
 
 class Bullet(DisplayObject):
-    VELOCITY = 7
-    BULLET_WIDTH, BULLET_HEIGHT = 10, 5
+    BULLET_WIDTH, BULLET_HEIGHT = 20, 6
 
     def __init__(self, start_x=0, start_y=0):
-        super().__init__(init_x=start_x, init_y=start_y, width=self.BULLET_WIDTH, height=self.BULLET_HEIGHT)
+        super().__init__(width=self.BULLET_WIDTH, height=self.BULLET_HEIGHT)
+        self.center = start_x, start_y
 
-    def move(self):
-        pass
+    def move(self, direction):
+        self.x += direction
 
 
 class Spaceship(DisplayObject):
@@ -68,30 +84,29 @@ class Spaceship(DisplayObject):
     SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
     def __init__(self, image, start_x=0, start_y=0):
-        super().__init__(init_x=start_x, init_y=start_y, width=self.SPACESHIP_WIDTH, height=self.SPACESHIP_HEIGHT)
+        super().__init__(init_x=start_x, init_y=start_y, width=self.SPACESHIP_WIDTH, height=self.SPACESHIP_WIDTH)
         self.image = pygame.image.load(os.path.join(ASSETS_FOLDER_PATH, image))
         self.transform_image((self.SPACESHIP_WIDTH, self.SPACESHIP_HEIGHT))
-        self.bullets = []
 
     def move_left(self):
         self.x -= self.VELOCITY 
-        self.rotate_image(-90)
+        #self.rotate_image(-90)
 
     def move_right(self):
         self.x += self.VELOCITY
-        self.rotate_image(90)
+        #self.rotate_image(90)
 
     def move_up(self):
         self.y -= self.VELOCITY
-        self.rotate_image(180)
+        #self.rotate_image(180)
 
     def move_down(self):
         self.y += self.VELOCITY
-        self.rotate_image(0)
+        #self.rotate_image(0)
 
 
 class ControlHandler:
-    def __init__(self, key_up, key_down, key_left, key_right, move_limit_area, key_shot, spaceship):
+    def __init__(self, key_up, key_down, key_left, key_right, move_limit_area, key_shot, player, spaceship, window):
         self.__moves_collection = [
             (key_up, "move_up", "top", operator.gt),
             (key_down, "move_down", "bottom", operator.lt), 
@@ -101,17 +116,19 @@ class ControlHandler:
         self.__key_shot = key_shot
         self.__spaceship = spaceship
         self.move_limit_area = move_limit_area
-    
-    @property
-    def move_limit_area(self):
-        try:
-            return self.__move_limit_area
-        except AttributeError:
-            return None
-
-    @move_limit_area.setter
-    def move_limit_area(self, rect: Rect):
-        self.__move_limit_area = rect
+        self.player = player
+        self.bullets_to_move = []
+        self.window = window
+        if self.player == "yellow":
+            self.direction = BULLET_VELOCITY
+            self.event_fire = YELLOW_FIRE
+            self.event_hit_enemy = RED_HIT
+        elif self.player == "red":
+            self.direction = -BULLET_VELOCITY
+            self.event_fire = RED_FIRE
+            self.event_hit_enemy = YELLOW_HIT
+        else:
+            raise AttributeError(f"Wrong player is set: {self.player}, expected players: yellow, red")
 
     def handle_move(self, keys_pressed):
         for key, method, side, operator in self.__moves_collection:
@@ -119,9 +136,17 @@ class ControlHandler:
                 getattr(self.__spaceship, method)()
 
     def handle_shot(self, event_key):
-        if event_key == self.__key_shot:
-            self.__spaceship.bullets.append(Bullet(*self.__spaceship.center))
-            print(self.__spaceship.bullets)
+        if event_key == self.__key_shot and len(self.bullets_to_move) < MAX_BULLETS:
+            bullet = Bullet(*self.__spaceship.image.get_rect(topleft=self.__spaceship.topleft).center)
+            self.bullets_to_move.append(bullet)
+            self.window.add_object(bullet)
+
+    def handle_bullets(self, vs):      
+        for bullet in self.bullets_to_move:
+            bullet.move(self.direction)
+            if bullet.colliderect(vs) or not self.window.surface.get_rect().contains(bullet):
+                self.bullets_to_move.remove(bullet)
+                self.window.remove_object(bullet)
 
 
 def main():
@@ -129,31 +154,35 @@ def main():
     win = Display(WIDTH, HEIGHT)
 
     yellow_spaceship = Spaceship("spaceship_yellow.png", 100, 300)
-    #yellow_spaceship.rotate_image(90)
+    yellow_spaceship.rotate_image(90)
 
     red_spaceship = Spaceship("spaceship_red.png", 700, 300)
-    #red_spaceship.rotate_image(-90)
+    red_spaceship.rotate_image(-90)
 
     win.add_object(yellow_spaceship)
     win.add_object(red_spaceship)
 
-    controler_1 = ControlHandler(
+    controller_1 = ControlHandler(
         pygame.K_w, 
         pygame.K_s, 
         pygame.K_a, 
         pygame.K_d,
         Rect(0, 0, BORDER_RECT.left, HEIGHT),
         pygame.K_LCTRL,
-        spaceship=yellow_spaceship)
+        player="yellow",
+        spaceship=yellow_spaceship,
+        window=win)
 
-    controler_2 = ControlHandler(
+    controller_2 = ControlHandler(
         pygame.K_UP, 
         pygame.K_DOWN, 
         pygame.K_LEFT, 
         pygame.K_RIGHT,
         Rect(BORDER_RECT.right, 0, BORDER_RECT.left, HEIGHT),
         pygame.K_RCTRL,
-        spaceship=red_spaceship)
+        player="red",
+        spaceship=red_spaceship,
+        window=win)
 
     run = True
     while run:
@@ -163,13 +192,16 @@ def main():
                 run = False
 
             if event.type == pygame.KEYDOWN:
-                controler_1.handle_shot(event.key)
-                controler_2.handle_shot(event.key)
+                controller_1.handle_shot(event.key)
+                controller_2.handle_shot(event.key)
 
         keys_pressed = pygame.key.get_pressed()
-        controler_1.handle_move(keys_pressed)
-        controler_2.handle_move(keys_pressed)
-              
+        controller_1.handle_move(keys_pressed)
+        controller_2.handle_move(keys_pressed)
+
+        controller_1.handle_bullets(vs=red_spaceship)
+        controller_2.handle_bullets(vs=yellow_spaceship)
+
         win.draw()
 
     pygame.quit()
